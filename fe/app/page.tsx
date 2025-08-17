f042381113d9a6212cbc4550c1472258ba319032
tranp6648 <tranp6648@gmail.com>
2025-08-17 16:17:10 +0700

    fix fe

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -1,9 +1,20 @@
+
+import HomeHighlights from "@/components/HomeHighlights";
+import MovieDetail from "@/components/MovieHero";
+import HeroBanner from "@/components/MovieHero";
+import MovieSlider from "@/components/MovieHero";
+import MovieCard from "@/components/MovieHero";
+import HomeBanner from "@/components/MovieHero";
+import RoPhimHeroBanner from "@/components/MovieHero";
 import Image from "next/image";
 
 export default function Home() {
-  return (
+ return (
+   
    <>
-   <h2>Home</h2>
+      <MovieSlider />
+      <HomeHighlights/>
    </>
+   
   );
 }
