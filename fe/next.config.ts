aa1f9fde3b0e5be7b13f9312db49aa1e807c9217
dangvanphucnghia <nghia812014@gmail.com>
2025-08-19 22:00:06 +0700

    update full general view by nghia

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -3,22 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'static.nutscdn.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'i.imgur.com' },
-      { protocol: 'https', hostname: 'image.tmdb.org' },
-      { protocol: 'https', hostname: 'cdn.myanimelist.net' },
-      { protocol: 'https', hostname: 'www.themoviedb.org' },
-      { protocol: 'https', hostname: 'www.themoviedb.com' },
-      { protocol: 'https', hostname: 'cdn.discordapp.com' },
-      { protocol: 'https', hostname: 'i.ytimg.com' },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "picsum.photos" },
-      // add any others you use:
-      // { protocol: 'https', hostname: 'image.tmdb.org' },
-    ],
-  },
+  remotePatterns: [
+    { protocol: 'https', hostname: 'res.cloudinary.com' },
+    { protocol: 'https', hostname: 'i.pravatar.cc' },
+    { protocol: 'https', hostname: 'image.tmdb.org' },
+    { protocol: 'https', hostname: 'cdn.myanimelist.net' },
+    { protocol: 'https', hostname: 'i.imgur.com' },
+    { protocol: 'https', hostname: 'static.nutscdn.com' },
+    { protocol: 'https', hostname: 'i.ytimg.com' },
+    { protocol: 'https', hostname: 'images.unsplash.com' },
+    { protocol: 'https', hostname: 'picsum.photos' },
+  ],
+},
+
+
 };
 
 export default nextConfig;
